5ffba76a106e06a122307842d4fed347e6dba7cf
Sukanya Walunj <walunjsukanya1@gmail.com>
2024-09-23 16:13:10 +0530

    commit by command

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React  sukanya 
+          Learn React
         </a>
       </header>
     </div>
